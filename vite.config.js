aca0cd8979ac0bc773c19624e108707f5b361327
krishna <YOUR_GMAIL@gmail.com>
2026-05-21 11:34:37 +0530

    PWA setup - Install as app iOS + Android

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,57 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['*.jpg', '*.webp', '*.png'],
+      manifest: {
+        name: 'Sadhna OS',
+        short_name: 'Sadhna OS',
+        description: 'ISKCON Sadhna Tracking App - All Glories to Srila Prabhupada',
+        theme_color: '#ff9933',
+        background_color: '#0f0a05',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/p13.jpg',
+            sizes: '192x192',
+            type: 'image/jpeg',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/p13.jpg',
+            sizes: '512x512',
+            type: 'image/jpeg',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,jpg,webp,png}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  ]
 })
